058a0748b593efc95aa999b5f3beb5321605996e
TVANSCHENDEL <tom.vanschendel@ynov.com>
2022-05-03 18:35:53 +0200

    [EDIT] fix some problems with devise and add login and logout buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  # http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  before_action :authenticate_user!
+
 end
